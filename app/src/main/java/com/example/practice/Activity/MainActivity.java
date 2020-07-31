067ff5b7a017bf83a478fad1cad8bdb733d12dfb
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-07-31 22:49:16 +0900

    スピナーで数字を選択、+-のボタンの数字を変更させ、その数字の分だけ足し引き出来るようにした。

--- app/src/main/java/com/example/practice/Activity/MainActivity.java
+++ app/src/main/java/com/example/practice/Activity/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.practice.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.practice.R;
 
 public class MainActivity extends AppCompatActivity {
 
     int count = 0;
+    int number = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,31 +24,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView textView = findViewById(R.id.textView);
-        Button addButton = findViewById(R.id.button);
-        Button minusButton = findViewById(R.id.button2);
+        final Button addButton = findViewById(R.id.button);
+        final Button minusButton = findViewById(R.id.button2);
+        Spinner mySpinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numbers_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        mySpinner.setAdapter(adapter);
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //ここから
+                String selectNumbers = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, selectNumbers, Toast.LENGTH_LONG);
+                number = Integer.parseInt(selectNumbers);
+                addButton.setText("+"+number);
+                minusButton.setText("-"+number);
+                //minusButton.setText(String.valueOf(number));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count += 1;
-                if(count%10 != 0){
-                textView.setText(String.valueOf(count));
+                count += number;
+                if (count % 10 != 0) {
+                    textView.setText(String.valueOf(count));
 
-                }else{
-                textView.setText(count+"\n十の倍数！");}
+                } else {
+                    textView.setText(count + "\n十の倍数！");
+                }
             }
         });
 
         minusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count -= 1;
-                if(count%10 != 0){
+                count -= number;
+                if (count % 10 != 0) {
                     textView.setText(String.valueOf(count));
-                }else{
-                    textView.setText(count+"\n十の倍数！");}
+                } else {
+                    textView.setText(count + "\n十の倍数！");
+                }
             }
-            });
-        }
-
+        });
     }
+
+}
